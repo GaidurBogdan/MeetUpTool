2f8a489e65edc92d19ddd2fe9300fc5c90365e0a
DESKTOP-F9IPFTE\Bog <bgaidur@gmail.com>
2019-02-06 16:45:45 +0200

    Created chat box + decreased header height by 10px
    Chat is only available on the index page at the moment. It will be generated from js later.

---
+++ scripts/home.js
@@ -0,0 +1,26 @@
+var chatBubbles = document.getElementsByClassName("chat__bubble");
+var chatContainer = document.getElementsByClassName("chat__container")[0];
+
+document.addEventListener('click', function (event) {
+
+	if (event.target.matches('.chat__bubble')) {
+        let bubble = event.target;
+
+        if (chatContainer.classList.contains("hidden")) {
+            chatContainer.classList.remove("hidden");
+        }
+        
+        if (bubble.classList.contains("highlighted")) {
+            chatContainer.classList.add("hidden");
+            bubble.classList.remove("highlighted");
+        } else {
+            for (var i=0; i < chatBubbles.length; i++)
+                chatBubbles[i].classList.remove("highlighted");
+            bubble.classList.add("highlighted");
+            chatContainer.id = bubble.id + "_container_id";
+        }
+    }
+
+	console.log(event.target);
+
+}, false);
